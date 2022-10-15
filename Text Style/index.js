9dc3fbd694bf75e9fd5f1c9d1de4dbe85602e31e
Suman Paik <sumanpaik.dev@gmail.com>
2022-10-15 21:18:23 +0530

    text style appearence style

---
+++ Text Style/index.js
@@ -0,0 +1,41 @@
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    section.forEach((i) => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - 150;
+        let height = i.offsetHeight;
+        let id = i.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            menu.forEach((link) => {
+                link.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
+            });
+        }
+    });
+};
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
