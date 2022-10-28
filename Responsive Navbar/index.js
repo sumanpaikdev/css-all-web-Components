37d3a38806bddcfedb76c321b6e1147e5d0514ab
Suman Paik <sumanpaik.dev@gmail.com>
2022-10-28 14:19:30 +0530

    I created a responsive navbar

---
+++ Responsive Navbar/index.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("mobileView").style.width = "100%"
+}
+function closeNav() {
+    document.getElementById("mobileView").style.width = "0%"
+}
---
+++ sidenav/index.js
@@ -0,0 +1,6 @@
+function openNav() {
+    // document.getElementById("mySidenav").style.width = "400px"
+}
+function closeNav() {
+    // document.getElementById("mySidenav").style.width = "0px"    
+}
